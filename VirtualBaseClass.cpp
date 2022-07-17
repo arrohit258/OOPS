20397aca2cbf240cc9516f3b2b349abbd2ba1598
Rohit Arora <63255932+arrohit258@users.noreply.github.com>
2022-07-17 22:00:46 +0530

    Create VirtualBaseClass.cpp

---
+++ VirtualBaseClass.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+using namespace std;
+
+class Student{
+	protected:
+	int roll_number;
+};
+class Exam : public virtual Student{
+	protected:
+	int score;
+};
+class Sports : public virtual Student{
+	protected:
+	int marks;
+};
+
+class Result : public Exam,public Sports{
+	protected:
+	int total;
+	public:
+	void set_params(int roll_no,int a,int b){
+		score=a;
+		marks=b;
+		roll_number=roll_no;
+		total=a+b;
+	}
+	void show(){
+		cout<<roll_number<<" "<<total<<endl;
+
+	}
+};
+int main()
+{
+    Result rohit;
+   rohit.set_params(420,50,60);
+   rohit.show();
+    return 0;
+}
