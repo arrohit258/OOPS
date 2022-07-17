b80af70c7a9503a013480a2fa9eb0ecc0b35cbff
Rohit Arora <63255932+arrohit258@users.noreply.github.com>
2022-07-17 22:13:30 +0530

    Update VirtualBaseClass.cpp

--- VirtualBaseClass.cpp
+++ VirtualBaseClass.cpp
@@ -4,23 +4,38 @@ using namespace std;
 class Student{
 	protected:
 	int roll_number;
+	public:
+	Student(){
+		cout<<"Student Created"<<endl;
+	}
 };
-class Exam : public virtual Student{//virtual keyword to avoid clash on roll_number when being accessed in Result
+class Exam : protected virtual Student{//virtual to avoid roll_number conflict as Exam and Sports dono se ayega
 	protected:
 	int score;
+	public:
+	Exam(){
+		cout<<"Exam created"<<endl;
+	}
 };
 class Sports : public virtual Student{
 	protected:
-	int marks;
+	int score;
+	public:
+	Sports(){
+		cout<<"Sports created"<<endl;
+	}
 };
 
-class Result : public Exam,public Sports{
+class Result : public Exam,protected Sports{
 	protected:
 	int total;
 	public:
+	Result(){
+		cout<<"Result created"<<endl;
+	}
 	void set_params(int roll_no,int a,int b){
-		score=a;
-		marks=b;
+		Exam::score=a; // Ambuiguity handle karne ko
+		Sports::score=b; //
 		roll_number=roll_no;
 		total=a+b;
 	}
@@ -36,3 +51,17 @@ int main()
    rohit.show();
     return 0;
 }
+
+/*
+OUTPUT 
+
+Student Created
+Exam created
+Sports created
+Result created
+420 110
+
+
+
+
+*/
